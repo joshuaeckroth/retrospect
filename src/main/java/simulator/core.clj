37c5917a9d418fde4490227619c77de08163ee99
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2010-08-05 09:59:07 -0400

    Progress towards a working tracking player (#14).
    Player now starts at the right time and shows some fields for parameters.

--- src/main/java/simulator/core.clj
+++ src/main/java/simulator/core.clj
@@ -3,12 +3,13 @@
   (:use [clojure.contrib.command-line :only (with-command-line)])
   (:use [simulator.runner :only (multiple-runs save-results)])
   (:use [simulator.tracking :as tracking :only (generate-params run)])
-  (:use [simulator.records :only (run-with-new-record)]))
+  (:use [simulator.records :only (run-with-new-record)])
+  (:use [simulator.tracking.player :only (start-player)]))
 
 (defn -main [& args]
   (with-command-line args
     "Simulator"
-    [[action "Action (run/plot/playback)"]
+    [[action "Action (run/plot/player)"]
      [problem "Problem" "tracking"]
      [recordsdir "Records directory" "c:/users/josh/documents/research/simulator/records"]]
     (case action
@@ -17,8 +18,9 @@
 	    (run-with-new-record recordsdir (tracking/generate-params) tracking/run))
 	  "plot"
 	  (println "Plot...")
-	  "playback"
-	  (println "Playback...")
+	  "player"
+	  (if (= problem "tracking") (start-player)
+        (println "Player only available for 'tracking' problem."))
 	  (println "No action given."))))
 
 
--- src/main/java/simulator/tracking/player.clj
+++ src/main/java/simulator/tracking/player.clj
@@ -1,9 +1,8 @@
 (ns simulator.tracking.player
   (:import (java.io BufferedWriter FileWriter))
-  (:import (java.awt Color Graphics Dimension))
+  (:import (java.awt Color Graphics Dimension GridBagLayout Insets))
   (:import (java.awt.image BufferedImage))
-  (:import (javax.swing JPanel JFrame)))
-
+  (:import (javax.swing JPanel JFrame JButton JTextField JLabel)))
 
 (def *width* 10)
 (def *height* 10)
@@ -13,6 +12,8 @@
 (def *time* 0)
 (def *event-log* nil)
 (def *grid* (vec (repeat (* *width* *height*) nil)))
+
+(def animator (agent nil))
 
 (defn grid-at [x y]
   (nth *grid* (+ (* y *width*) x)))
@@ -47,18 +48,18 @@
 
 (defn update-grid []
   (dorun (for [e (filter #(= (:time %) *time*) *event-log*)]
-	   (let [{x :x y :y} (if (= (type e) simulator.types.events.EventMove) (:newpos e) (:pos e))]
-	     (def *grid* (assoc *grid* (+ (* y *width*) x) e)))))
+           (let [{x :x y :y} (if (= (type e) simulator.types.events.EventMove) (:newpos e) (:pos e))]
+             (def *grid* (assoc *grid* (+ (* y *width*) x) e)))))
   (dorun (for [x (range *width*) y (range *height*)]
-	   (if (and (not (nil? (grid-at x y))) (< (:time (grid-at x y)) (- *time* 3)))
-	     (def *grid* (assoc *grid* (+ (* y *width*) x) nil))))))
+           (if (and (not (nil? (grid-at x y))) (< (:time (grid-at x y)) (- *time* 3)))
+             (def *grid* (assoc *grid* (+ (* y *width*) x) nil))))))
 
 (defn render [g]
   (let [img (new BufferedImage
 		 (* *grid-cell-size* *width*)
 		 (* *grid-cell-size* *height*)
 		 (. BufferedImage TYPE_INT_ARGB))
-	bg (. img (getGraphics))]
+        bg (. img (getGraphics))]
     (doto bg
       (.setColor (. Color white))
       (.fillRect 0 0 (. img (getWidth)) (. img (getHeight))))
@@ -67,19 +68,94 @@
     (. g (drawImage img 0 0 nil))
     (. bg (dispose))))
 
-(comment
+;;; from http://stuartsierra.com/2010/01/05/taming-the-gridbaglayout
+
+(defmacro set-grid! [constraints field value]
+  `(set! (. ~constraints ~(symbol (name field)))
+         ~(if (keyword? value)
+            `(. java.awt.GridBagConstraints
+                ~(symbol (name value)))
+            value)))
+
+;;; from http://stuartsierra.com/2010/01/05/taming-the-gridbaglayout
+
+(defmacro grid-bag-layout [container & body]
+  (let [c (gensym "c")
+        cntr (gensym "cntr")]
+    `(let [~c (new java.awt.GridBagConstraints)
+           ~cntr ~container]
+       ~@(loop [result '() body body]
+           (if (empty? body)
+             (reverse result)
+             (let [expr (first body)]
+               (if (keyword? expr)
+                 (recur (cons `(set-grid! ~c ~expr
+                                          ~(second body))
+                              result)
+                        (next (next body)))
+                 (recur (cons `(.add ~cntr ~expr ~c)
+                              result)
+                        (next body)))))))))
+
+(def *gridpanel*
+     (doto (proxy [JPanel] []
+	     (paint [g] (render g)))
+       (.setPreferredSize
+	(new Dimension
+	     (* *grid-cell-size* *width*)
+	     (* *grid-cell-size* *height*)))))
+
 (def *panel*
-  (doto (proxy [JPanel] []
-	  (paint [g] (render g)))
-    (.setPreferredSize
-     (new Dimension
-	  (* *grid-cell-size* *width*)
-	  (* *grid-cell-size* *height*)))))
+     (doto (JPanel. (GridBagLayout.))
+       (grid-bag-layout
+	:fill :BOTH, :insets (Insets. 5 5 5 5)
+	:gridx 0, :gridy 0, :gridheight 8
+	*gridpanel*
 
-(def *frame*
-  (doto (new JFrame) (.add *panel*) .pack .show))
+	:gridx 1, :gridy 0, :gridheight 1
+	(JLabel. "Steps:")
+	:gridx 2, :gridy 0
+	(JTextField. 5)
 
-(def animator (agent nil))
+	:gridx 1, :gridy 1
+	(JLabel. "NumberEntities:")
+	:gridx 2, :gridy 1
+	(JTextField. 5)
+
+	:gridx 1, :gridy 2
+	(JLabel. "WalkSize:")
+	:gridx 2, :gridy 2
+	(JTextField. 5)
+
+	:gridx 1, :gridy 3
+	(JLabel. "GridWidth:")
+	:gridx 2, :gridy 3
+	(JTextField. 5)
+
+	:gridx 1, :gridy 4
+	(JLabel. "GridHeight:")
+	:gridx 2, :gridy 4
+	(JTextField. 5)
+
+	:gridx 1, :gridy 5
+	(JLabel. "Strategy:")
+	:gridx 2, :gridy 5
+	(JTextField. 5)
+
+	:gridx 1, :gridy 6
+	(JLabel. "SensorCoverage:")
+	:gridx 2, :gridy 6
+	(JTextField. 5)
+
+	:gridy 7, :gridwidth 2, :gridheight :REMAINDER
+	(JPanel.))))
+
+(defn start-player []
+  (doto (JFrame. "Tracking player")
+    (.setContentPane *panel*)
+    ;;(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+    (.pack)
+    (.show)))
 
 (defn animation [_]
   (when *running*
@@ -89,7 +165,6 @@
   (def *time* (inc *time*))
   (if (> *time* 50) (def *running* false))
   nil)
-)
 
 ;;; add sensor coverage overlays in visual grid
 
