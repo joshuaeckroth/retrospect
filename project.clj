1dcafcfb2f3d3843becd754a611a0df41b48e97e
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2010-08-31 16:23:42 -0400

    Using clj-stacktrace in main file.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
 		 [incanter "1.2.3-SNAPSHOT"]
-		 [org.clojars.choas/clojure-hadoop "1.2.1-SNAPSHOT"]]
+		 [org.clojars.choas/clojure-hadoop "1.2.1-SNAPSHOT"]
+		 [clj-stacktrace "0.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :main simulator.core
   :source-path "src/main/java"
--- src/main/java/simulator/core.clj
+++ src/main/java/simulator/core.clj
@@ -5,36 +5,39 @@
   (:use [simulator.problems.tracking.player :as tracking-player :only (start-player)])
   (:use [simulator.records
 	 :only (run-with-new-record list-records prepare-hadoop cleanup-hadoop-results)])
-  (:use [simulator.runners.hadoop :only (run-hadoop)]))
+  (:use [simulator.runners.hadoop :only (run-hadoop)])
+  (:use [clj-stacktrace.repl :only (pst+)]))
 
 (defn -main [& args]
-  (with-command-line args
-    "Simulator"
-    [[action "Action (run/list/player/prepare-hadoop/hadoop/clean-hadoop)"]
-     [problem "Problem" "tracking"]
-     [paramsfile "Parameters XML file" "params.xml"]
-     [recordsdir "Records directory" "c:/users/josh/documents/research/simulator-records"]
-     [nthreads "Number of threads" "4"]
-     [input "Hadoop input" ""]
-     [output "Hadoop output" ""]]
-    (let [nthreads (Integer/parseInt nthreads)
-	  prob tracking-problem]
-      (case action
-	    "run"
-	    (run-with-new-record prob paramsfile recordsdir nthreads)
-	    "prepare-hadoop"
-	    (prepare-hadoop prob paramsfile recordsdir)
-	    "hadoop"
-	    (run-hadoop prob input output args)
-	    "clean-hadoop"
-	    (cleanup-hadoop-results prob recordsdir
-				    (str recordsdir "/results-hadoop.csv")
-				    (str recordsdir "/results.csv"))
-	    "list"
-	    (list-records recordsdir)
-	    "player"
-	    (if (= problem "tracking") (tracking-player/start-player)
-		(println "Player only available for 'tracking' problem."))
-	    (println "No action given.")))))
+  (try
+    (with-command-line args
+      "Simulator"
+      [[action "Action (run/list/player/prepare-hadoop/hadoop/clean-hadoop)"]
+       [problem "Problem" "tracking"]
+       [paramsfile "Parameters XML file" "params.xml"]
+       [recordsdir "Records directory" "c:/users/josh/documents/research/simulator-records"]
+       [nthreads "Number of threads" "4"]
+       [input "Hadoop input" ""]
+       [output "Hadoop output" ""]]
+      (let [nthreads (Integer/parseInt nthreads)
+	    prob tracking-problem]
+	(case action
+	      "run"
+	      (run-with-new-record prob paramsfile recordsdir nthreads)
+	      "prepare-hadoop"
+	      (prepare-hadoop prob paramsfile recordsdir)
+	      "hadoop"
+	      (run-hadoop prob input output args)
+	      "clean-hadoop"
+	      (cleanup-hadoop-results prob recordsdir
+				      (str recordsdir "/results-hadoop.csv")
+				      (str recordsdir "/results.csv"))
+	      "list"
+	      (list-records recordsdir)
+	      "player"
+	      (if (= problem "tracking") (tracking-player/start-player)
+		  (println "Player only available for 'tracking' problem."))
+	      (println "No action given."))))
+    (catch Exception e (pst+ e))))
 
 
