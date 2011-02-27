13a5a5291263ad1ba170da6c5cef82508e7e1701
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-02-27 10:19:25 -0500

    Removed some lein stuff now that cake is being used.

--- project.clj
+++ project.clj
@@ -6,13 +6,5 @@
                  [org.clojars.overtone/vijual "0.2.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [marginalia "0.5.0"]
-                     [clj-stacktrace "0.2.1"]]
-  :main retrospect.core
-  :source-path "src"
-  :test-path "test"
-  :library-path "lib"
-  :resources-path "resources"
-  :target-dir "target"
-  :keep-non-project-classes true)
+                     [clj-stacktrace "0.2.1"]])
 
-; :jvm-opts ["-agentlib:jdwp=transport=dt_socket,address=8021,server=y,suspend=n"]
