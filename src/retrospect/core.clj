6262adee7f33066be20b50edf751913bdb65c737
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-03-02 06:48:09 -0500

    Removed swank server & repl code.

--- src/retrospect/core.clj
+++ src/retrospect/core.clj
@@ -4,8 +4,7 @@
   (:use [retrospect.problems.tracking.problem :only (tracking-problem)])
   ;(:use [retrospect.problems.circuit.problem :only (circuit-problem)])
   (:use [retrospect.records :only [run-with-new-record list-records]])
-  (:use [retrospect.player :only (start-player)])
-  (:require [swank.swank]))
+  (:use [retrospect.player :only (start-player)]))
 
 (defn -main [& args]
   (with-command-line args
@@ -17,22 +16,20 @@
      [record "Record" ""]
      [nthreads "Number of threads" "1"]
      [repetitions "Number of repetitions" "10"]
-     [monitor "Activate monitor?" "false"]]
+     [monitor? "Activate monitor?" "false"]]
     (let [nthreads (Integer/parseInt nthreads)
           repetitions (Integer/parseInt repetitions)
-          monitor (Boolean/parseBoolean monitor)
+          monitor? (Boolean/parseBoolean monitor?)
           prob (cond (= problem "tracking") tracking-problem
                      ;(= problem "circuit") circuit-problem
                      :else nil)]
       (case action
             "run"
-            (run-with-new-record prob paramsfile recordsdir nthreads monitor repetitions)
+            (run-with-new-record prob paramsfile recordsdir nthreads monitor? repetitions)
             "list"
             (list-records recordsdir)
             "player"
-            (do
-              (swank.swank/start-repl 4006)
-              (start-player prob))
+            (start-player prob)
 
             (println "No action given.")))))
 
--- src/retrospect/problems/tracking/repl.clj
+++
@@ -1,9 +0,0 @@
-(ns retrospect.problems.tracking.repl
-  (:use retrospect.repl))
-
-(def *truedata*)
-
-(defn update-truedata
-  [td]
-  (def *truedata* td))
-
