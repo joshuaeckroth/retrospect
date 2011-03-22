a85d454f7af86e46383754cfffc2e3a056919c72
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-03-22 15:35:46 -0400

    More tracking-prepared test cases.

--- test/retrospect/problems/tracking/test_prepared.clj
+++ test/retrospect/problems/tracking/test_prepared.clj
@@ -1,12 +1,30 @@
 (ns retrospect.problems.tracking.test-prepared
-  (:use midje.sweet)
-  (:use [retrospect.workspaces :only [last-id]])
+  (:use [midje.sweet])
+  (:use [retrospect.workspaces :only [last-id get-conf]])
+  (:use [retrospect.epistemicstates :only [previous-ep-state]])
   (:use [retrospect.onerun :only [init-one-run-state]])
-  (:use [retrospect.problem :only [run-simulation]])
-  (:use retrospect.problems.tracking.prepared)
-  (:use [retrospect.problems.tracking.problem :only [tracking-problem]]))
+  (:use [retrospect.problem :only [run-simulation run-simulation-step]])
+  (:use [retrospect.problems.tracking.prepared])
+  (:use [retrospect.problems.tracking.problem :only [tracking-problem]])
+  (:use [retrospect.confidences]))
 
-(defn run-prepared
+(defn run-for-or-state
+  [prepared]
+  (let [params (:params prepared)
+        meta-abduction (:MetaAbduction params)
+        lazy (:Lazy params)
+        sensors (:sensors prepared)
+        truedata (:truedata prepared)
+        or-state (init-one-run-state meta-abduction lazy sensors
+                                     ((:gen-problem-data-fn tracking-problem)
+                                      sensors params))]
+    (binding [last-id 0]
+      (loop [ors or-state]
+        (if (>= (:time (:ep-state ors)) (:Steps params)) ors
+            (recur
+             (run-simulation-step tracking-problem truedata ors params false true)))))))
+
+(defn run-for-results
   [prepared]
   (let [params (:params prepared)
         meta-abduction (:MetaAbduction params)
@@ -19,11 +37,16 @@
     (binding [last-id 0]
       (run-simulation tracking-problem truedata or-state params false))))
 
-(let [results (run-prepared intersection-ambiguity)]
+(let [results (run-for-results intersection-ambiguity)]
   (facts (:PercentEventsCorrect results) => (roughly 100.0)
          (:MetaAbductions results) => 1))
 
-(let [results (run-prepared intersection-ambiguity-nometa)]
+(let [results (run-for-results intersection-ambiguity-nometa)]
   (facts (:PercentEventsCorrect results) => (roughly 0.0)
          (:MetaAbductions results) => 0))
+
+(let [or-state (run-for-or-state simple-disappearance)
+      prev-ep (previous-ep-state (:ep-state-tree or-state))]
+  (facts (:unexplained (:final (:log (:workspace prev-ep)))) => (just "THW6")
+         (get-conf (:workspace prev-ep)) => VERY-IMPLAUSIBLE))
 
