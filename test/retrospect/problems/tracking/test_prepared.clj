1b8d733f61a3221b413a3b66e7622e6cbd5184d7
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-03-31 16:50:59 -0400

    Fixed prepared test cases for change in how entities are reported (colors).

--- test/retrospect/problems/tracking/test_prepared.clj
+++ test/retrospect/problems/tracking/test_prepared.clj
@@ -54,28 +54,28 @@
   (facts (:PercentEventsCorrect results) => (roughly 100.0)
          (:MeanTimeWithLabel results) => (roughly 2.0)
          (paths-str (:paths (:problem-data (:ep-state or-state))))
-         => "A:[4,0@0 4,2@1 ]\nB:[4,2@1 2,4@2 2,6@3 ]\nC:[4,2@1 6,4@2 6,6@3 ]"))
+         => "A (red): [4,0@0 4,2@1 ]\nB (red): [4,2@1 2,4@2 2,6@3 ]\nC (red): [4,2@1 6,4@2 6,6@3 ]"))
 
 (let [or-state (run-for-or-state split-ambiguity-2)
       results (last (:results or-state))]
   (facts (:PercentEventsCorrect results) => (roughly 100.0)
          (:MeanTimeWithLabel results) => (roughly 2.0)
          (paths-str (:paths (:problem-data (:ep-state or-state))))
-         => "A:[1,0@0 1,1@1 ]\nB:[6,0@0 6,1@1 ]\nC:[1,1@1 0,2@2 0,3@3 ]\nD:[1,1@1 2,2@2 2,3@3 ]\nE:[6,1@1 5,2@2 5,3@3 ]\nF:[6,1@1 7,2@2 7,3@3 ]"))
+         => "A (red): [1,0@0 1,1@1 ]\nB (blue): [6,0@0 6,1@1 ]\nC (red): [1,1@1 0,2@2 0,3@3 ]\nD (red): [1,1@1 2,2@2 2,3@3 ]\nE (blue): [6,1@1 5,2@2 5,3@3 ]\nF (blue): [6,1@1 7,2@2 7,3@3 ]"))
 
 (let [or-state (run-for-or-state merge-ambiguity)
       results (last (:results or-state))]
   (facts (:PercentEventsCorrect results) => (roughly 100.0)
          (:MeanTimeWithLabel results) => (roughly 1.66 0.1)
          (paths-str (:paths (:problem-data (:ep-state or-state))))
-         => "A:[4,0@0 4,2@1 5,4@2 ]\nB:[6,0@0 6,2@1 5,4@2 ]\nC:[5,4@2 5,6@3 ]"))
+         => "A (red): [4,0@0 4,2@1 5,4@2 ]\nB (red): [6,0@0 6,2@1 5,4@2 ]\nC (red): [5,4@2 5,6@3 ]"))
 
 (let [or-state (run-for-or-state merge-ambiguity-2)
       results (last (:results or-state))]
   (facts (:PercentEventsCorrect results) => (roughly 100.0)
          (:MeanTimeWithLabel results) => (roughly 1.66 0.1)
          (paths-str (:paths (:problem-data (:ep-state or-state))))
-         => "A:[0,0@0 0,2@1 1,4@2 ]\nB:[2,0@0 2,2@1 1,4@2 ]\nC:[7,0@0 7,2@1 8,4@2 ]\nD:[9,0@0 9,2@1 8,4@2 ]\nE:[8,4@2 8,6@3 ]\nF:[1,4@2 1,6@3 ]"))
+         => "A (red): [0,0@0 0,2@1 1,4@2 ]\nB (red): [2,0@0 2,2@1 1,4@2 ]\nC (blue): [7,0@0 7,2@1 8,4@2 ]\nD (blue): [9,0@0 9,2@1 8,4@2 ]\nE (blue): [8,4@2 8,6@3 ]\nF (red): [1,4@2 1,6@3 ]"))
 
 (let [or-state (run-for-or-state split-merge)
       results (last (:results or-state))]
@@ -83,5 +83,5 @@
          (:MeanTimeWithLabel results) => (roughly 1.75)
          (:MeanLabelCounts results) => (roughly 3.50)
          (paths-str (:paths (:problem-data (:ep-state or-state))))
-         => "A:[5,0@0 5,1@1 ]\nB:[5,1@1 3,3@2 3,4@3 5,5@4 ]\nC:[5,1@1 7,3@2 7,4@3 5,5@4 ]\nD:[5,5@4 5,6@5 ]"))
+         => "A (red): [5,0@0 5,1@1 ]\nB (red): [5,1@1 3,3@2 3,4@3 5,5@4 ]\nC (red): [5,1@1 7,3@2 7,4@3 5,5@4 ]\nD (red): [5,5@4 5,6@5 ]"))
 
