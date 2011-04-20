9ead210d5bfc421132edd8ad7cc5683f32901ecb
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-04-19 23:34:23 -0400

    Added a new prepared case (but no test case yet).

--- src/retrospect/problems/tracking/prepared.clj
+++ src/retrospect/problems/tracking/prepared.clj
@@ -1,6 +1,7 @@
 (ns retrospect.problems.tracking.prepared
   (:use [retrospect.problems.tracking.grid :only
-         [new-grid grid-add grid-move grid-at update-all-entity-times find-entity]])
+         [new-grid grid-add grid-move grid-at
+          update-all-entity-times find-entity]])
   (:use [retrospect.problems.tracking.sensors :only [new-sensor]])
   (:use [retrospect.colors]))
 
@@ -20,7 +21,8 @@
       (if (= t steps) td
           (let [g (last td)
                 g-new-es
-                (reduce (fn [grid e] (grid-add grid (:x (meta e)) (:y (meta e)) e))
+                (reduce (fn [grid e]
+                          (grid-add grid (:x (meta e)) (:y (meta e)) e))
                         (if (= t 0) (new-grid width height) g)
                         (filter (fn [e] (= (:time (meta e)) t)) (keys paths)))
                 g-moves
@@ -31,7 +33,8 @@
                                       :y (:y (get (paths e) t))
                                       :ox (:x (get (paths e) (dec t)))
                                       :oy (:y (get (paths e) (dec t)))})
-                             (filter (fn [e] (< (:time (meta e)) t)) (keys paths))))
+                             (filter (fn [e] (< (:time (meta e)) t))
+                                     (keys paths))))
                 g-time (update-all-entity-times g-moves t)]
             (recur (inc t) (conj td g-time)))))))
 
@@ -44,9 +47,10 @@
   [[id color time & xys]]
   (let [xy-pairs (partition 2 xys)
         path (zipmap (range time (+ time (count xy-pairs)))
-                     (map (fn [pair] {:x (first pair) :y (second pair)}) xy-pairs))]
+                     (map (fn [pair] {:x (first pair) :y (second pair)})
+                          xy-pairs))]
     [(new-entity id (ffirst xy-pairs) (second (first xy-pairs)) color time)
-      path]))
+     path]))
 
 (defn entity-paths
   [& eps]
@@ -54,25 +58,28 @@
     (apply hash-map es-with-paths)))
 
 (def simple-disappearance
-  (let [params (merge basic-params {:Steps 2 :SensorCoverage 50 :SensorSeesColor 0})]
-    {:params params
-     :sensors [(new-sensor (keyword "right") 5 9 0 9 false)]
-     :truedata (build-truedata params (entity-paths ["1" red  0 5,4 4,4]
-                                                    ["2" blue 0 5,5 5,4]))}))
+     (let [params (merge basic-params {:Steps 2 :SensorCoverage 50
+                                       :SensorSeesColor 0})]
+       {:params params
+        :sensors [(new-sensor (keyword "right") 5 9 0 9 false)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 5,4 4,4]
+                                        ["2" blue 0 5,5 5,4]))}))
 
 (def color-update
-  (let [params (merge basic-params {:Steps 3 :StepsBetween 3 :MaxWalk 10})]
-    {:params params
-     :sensors [(new-sensor (keyword "1") 0 3 0 9 false)
-               (new-sensor (keyword "2g") 4 7 0 9 true)
-               (new-sensor (keyword "3") 8 9 0 9 false)]
-     :truedata (build-truedata params (entity-paths ["1" red 0 9,5 5,5 2,5]))}))
+     (let [params (merge basic-params {:Steps 3 :StepsBetween 3 :MaxWalk 10})]
+       {:params params
+        :sensors [(new-sensor (keyword "1") 0 3 0 9 false)
+                  (new-sensor (keyword "2g") 4 7 0 9 true)
+                  (new-sensor (keyword "3") 8 9 0 9 false)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red 0 9,5 5,5 2,5]))}))
 
 (def color-update-2
-  (let [{params :params sensors :sensors truedata :truedata} color-update]
-    {:params (assoc params :StepsBetween 1)
-     :sensors sensors
-     :truedata truedata}))
+     (let [{params :params sensors :sensors truedata :truedata} color-update]
+       {:params (assoc params :StepsBetween 1)
+        :sensors sensors
+        :truedata truedata}))
 
 (def gray-in-range
      (let [params (merge basic-params {:Steps 3 :MaxWalk 10})]
@@ -80,58 +87,81 @@
         :sensors [(new-sensor (keyword "top") 0 9 0 3 true)
                   (new-sensor (keyword "middle-gray") 0 9 4 6 false)
                   (new-sensor (keyword "bottom") 0 9 7 9 true)]
-        :truedata (build-truedata params (entity-paths ["1" red  0 3,0 3,5 3,9]
-                                                       ["2" blue 0 7,0 7,5 7,9]))}))
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 3,0 3,5 3,9]
+                                        ["2" blue 0 7,0 7,5 7,9]))}))
 
 (def intersection-ambiguity
-  (let [params (merge basic-params {:Steps 4 :SensorSeesColor 80 :MaxWalk 3
-                                    :MetaAbduction true})]
-    {:params params
-     :sensors [(new-sensor (keyword "left") 0 2 0 9 true)
-               (new-sensor (keyword "middle") 3 4 0 9 false)
-               (new-sensor (keyword "right") 5 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red  0 5,7 4,4 2,3 0,3]
-                                                    ["2" blue 0 5,4 4,7 2,8 0,8]))}))
+     (let [params (merge basic-params {:Steps 4 :SensorSeesColor 80 :MaxWalk 3
+                                       :MetaAbduction true})]
+       {:params params
+        :sensors [(new-sensor (keyword "left") 0 2 0 9 true)
+                  (new-sensor (keyword "middle") 3 4 0 9 false)
+                  (new-sensor (keyword "right") 5 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 5,7 4,4 2,3 0,3]
+                                        ["2" blue 0 5,4 4,7 2,8 0,8]))}))
 
 (def intersection-ambiguity-nometa
-  (assoc-in intersection-ambiguity [:params :MetaAbduction] false))
+     (assoc-in intersection-ambiguity [:params :MetaAbduction] false))
 
 (def intersection-ambiguity-nometa-allatonce
-  (-> intersection-ambiguity
-      (assoc-in [:params :MetaAbduction] false)
-      (assoc-in [:params :StepsBetween] 4)))
+     (-> intersection-ambiguity
+         (assoc-in [:params :MetaAbduction] false)
+         (assoc-in [:params :StepsBetween] 4)))
+
+(def intersection-ambiguity-many-allatonce
+     (let [params (merge basic-params {:Steps 10 :SensorSeesColor 80 :MaxWalk 10
+                                       :StepsBetween 10 :GridWidth 30
+                                       :GridHeight 30})]
+       {:params params
+        :sensors [(new-sensor (keyword "1") 0 4 0 29 true)
+                  (new-sensor (keyword "1g") 5 9 0 29 false)
+                  (new-sensor (keyword "2") 10 14 0 29 true)
+                  (new-sensor (keyword "2g") 15 19 0 29 false)
+                  (new-sensor (keyword "3") 20 24 0 29 true)
+                  (new-sensor (keyword "3g") 25 29 0 29 false)]
+        :truedata
+        (build-truedata
+         params (entity-paths
+                 ["1" red  0 29,0 27,3 24,5 20,3 15,3 14,2 10,4 11,1 5,0 0,0]
+                 ["2" blue 0 29,2 23,3 20,6 20,2 18,7 14,2 11,8 9,9 4,5 0,4]))}))
 
 (def split-ambiguity
-  (let [params (merge basic-params {:Steps 4 :MaxWalk 2})]
-    {:params params
-     :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red 0 4,0 4,2 6,4 6,6]
-                                                    ["2" red 0 4,0 4,2 2,4 2,6]))}))
+     (let [params (merge basic-params {:Steps 4 :MaxWalk 2})]
+       {:params params
+        :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red 0 4,0 4,2 6,4 6,6]
+                                        ["2" red 0 4,0 4,2 2,4 2,6]))}))
 
 (def split-ambiguity-2
-  (let [params (merge basic-params {:Steps 4 :MaxWalk 1})]
-    {:params params
-     :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red  0 1,0 1,1 0,2 0,3]
-                                                    ["2" red  0 1,0 1,1 2,2 2,3]
-                                                    ["3" blue 0 6,0 6,1 5,2 5,3]
-                                                    ["4" blue 0 6,0 6,1 7,2 7,3]))}))
+     (let [params (merge basic-params {:Steps 4 :MaxWalk 1})]
+       {:params params
+        :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 1,0 1,1 0,2 0,3]
+                                        ["2" red  0 1,0 1,1 2,2 2,3]
+                                        ["3" blue 0 6,0 6,1 5,2 5,3]
+                                        ["4" blue 0 6,0 6,1 7,2 7,3]))}))
 
 (def merge-ambiguity
-  (let [params (merge basic-params {:Steps 4 :MaxWalk 3})]
-    {:params params
-     :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red 0 4,0 4,2 5,4 5,6]
-                                                    ["2" red 0 6,0 6,2 5,4 5,6]))}))
+     (let [params (merge basic-params {:Steps 4 :MaxWalk 3})]
+       {:params params
+        :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red 0 4,0 4,2 5,4 5,6]
+                                        ["2" red 0 6,0 6,2 5,4 5,6]))}))
 
 (def merge-ambiguity-2
-  (let [params (merge basic-params {:Steps 4 :MaxWalk 3})]
-    {:params params
-     :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red  0 0,0 0,2 1,4 1,6]
-                                                    ["2" red  0 2,0 2,2 1,4 1,6]
-                                                    ["3" blue 0 7,0 7,2 8,4 8,6]
-                                                    ["4" blue 0 9,0 9,2 8,4 8,6]))}))
+     (let [params (merge basic-params {:Steps 4 :MaxWalk 3})]
+       {:params params
+        :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 0,0 0,2 1,4 1,6]
+                                        ["2" red  0 2,0 2,2 1,4 1,6]
+                                        ["3" blue 0 7,0 7,2 8,4 8,6]
+                                        ["4" blue 0 9,0 9,2 8,4 8,6]))}))
 
 (def merge-ambiguity-gray
      (let [params (merge basic-params {:Steps 6 :MaxWalk 3})]
@@ -139,26 +169,28 @@
         :sensors [(new-sensor (keyword "top") 0 9 0 3 true)
                   (new-sensor (keyword "middle-gray") 0 9 4 6 false)
                   (new-sensor (keyword "bottom") 0 9 7 9 true)]
-        :truedata (build-truedata params
-                                  (entity-paths ["1" red  0 3,0 3,2 5,4 5,6 3,8 3,9]
-                                                ["2" blue 0 7,0 7,2 5,4 5,6 7,8 7,9]))}))
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 3,0 3,2 5,4 5,6 3,8 3,9]
+                                        ["2" blue 0 7,0 7,2 5,4 5,6 7,8 7,9]))}))
 
 (def split-merge
-  (let [params (merge basic-params {:Steps 6 :MaxWalk 2})]
-    {:params params
-     :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red 0 5,0 5,1 7,3 7,4 5,5 5,6]
-                                                    ["2" red 0 5,0 5,1 3,3 3,4 5,5 5,6]))}))
+     (let [params (merge basic-params {:Steps 6 :MaxWalk 2})]
+       {:params params
+        :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red 0 5,0 5,1 7,3 7,4 5,5 5,6]
+                                        ["2" red 0 5,0 5,1 3,3 3,4 5,5 5,6]))}))
 
 (def split-merge-allatonce
-  (assoc-in split-merge [:params :StepsBetween] 6))
+     (assoc-in split-merge [:params :StepsBetween] 6))
 
 (def split-non-ambiguity
-  (let [params (merge basic-params {:Steps 4})]
-    {:params params
-     :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red  0 4,3 4,4 5,5 5,6]
-                                                    ["2" blue 0 4,3 4,4 3,5 3,6]))}))
+     (let [params (merge basic-params {:Steps 4})]
+       {:params params
+        :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 4,3 4,4 5,5 5,6]
+                                        ["2" blue 0 4,3 4,4 3,5 3,6]))}))
 
 (def split-merge-gray
      (let [params (merge basic-params {:Steps 6 :MaxWalk 5})]
@@ -166,40 +198,42 @@
         :sensors [(new-sensor (keyword "top") 0 9 0 3 true)
                   (new-sensor (keyword "middle-gray") 0 9 4 6 false)
                   (new-sensor (keyword "bottom") 0 9 7 9 true)]
-        :truedata (build-truedata params
-                                  (entity-paths ["1" red  0 3,0 3,3 5,4 5,6 3,8 3,9]
-                                                ["2" blue 0 5,0 5,3 5,4 5,6 7,8 7,9]
-                                                ["3" red  0 7,0 7,3 5,4 5,6 5,8 5,9]))}))
+        :truedata (build-truedata
+                   params (entity-paths ["1" red  0 3,0 3,3 5,4 5,6 3,8 3,9]
+                                        ["2" blue 0 5,0 5,3 5,4 5,6 7,8 7,9]
+                                        ["3" red  0 7,0 7,3 5,4 5,6 5,8 5,9]))}))
 
 (def march
-  (let [params (merge basic-params {:Steps 6 :MaxWalk 1 :MetaAbduction true})]
-    {:params params
-     :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
-     :truedata (build-truedata params (entity-paths ["1" red 0 0,5 1,5 2,5 3,5 4,5 5,5]
-                                                    ["2" red 0 1,5 2,5 3,5 4,5 5,5 6,5]
-                                                    ["3" red 0 2,5 3,5 4,5 5,5 6,5 7,5]
-                                                    ["4" red 0 3,5 4,5 5,5 6,5 7,5 8,5]
-                                                    ["5" red 0 4,5 5,5 6,5 7,5 8,5 9,5]))}))
+     (let [params (merge basic-params {:Steps 6 :MaxWalk 1 :MetaAbduction true})]
+       {:params params
+        :sensors [(new-sensor (keyword "x") 0 9 0 9 true)]
+        :truedata (build-truedata
+                   params (entity-paths ["1" red 0 0,5 1,5 2,5 3,5 4,5 5,5]
+                                        ["2" red 0 1,5 2,5 3,5 4,5 5,5 6,5]
+                                        ["3" red 0 2,5 3,5 4,5 5,5 6,5 7,5]
+                                        ["4" red 0 3,5 4,5 5,5 6,5 7,5 8,5]
+                                        ["5" red 0 4,5 5,5 6,5 7,5 8,5 9,5]))}))
 
 (def march-aao
      (assoc-in march [:params :StepsBetween] 6))
 
 (def prepared-map
-  (sorted-map "color-update" color-update
-              "color-update-2" color-update-2
-              "gray-in-range" gray-in-range
-              "intersect" intersection-ambiguity
-              "intersect-nom" intersection-ambiguity-nometa
-              "intersect-nom-aao" intersection-ambiguity-nometa-allatonce
-              "march" march
-              "march-aao" march-aao
-              "merge" merge-ambiguity
-              "merge-2" merge-ambiguity-2
-              "merge-gray" merge-ambiguity-gray
-              "simple-dis" simple-disappearance
-              "split" split-ambiguity
-              "split-2" split-ambiguity-2
-              "split-merge" split-merge
-              "split-merge-aao" split-merge-allatonce
-              "split-merge-gray" split-merge-gray
-              "split-non-am" split-non-ambiguity))
+     (sorted-map "color-update" color-update
+                 "color-update-2" color-update-2
+                 "gray-in-range" gray-in-range
+                 "intersect" intersection-ambiguity
+                 "intersect-many-aao" intersection-ambiguity-many-allatonce
+                 "intersect-nom" intersection-ambiguity-nometa
+                 "intersect-nom-aao" intersection-ambiguity-nometa-allatonce
+                 "march" march
+                 "march-aao" march-aao
+                 "merge" merge-ambiguity
+                 "merge-2" merge-ambiguity-2
+                 "merge-gray" merge-ambiguity-gray
+                 "simple-dis" simple-disappearance
+                 "split" split-ambiguity
+                 "split-2" split-ambiguity-2
+                 "split-merge" split-merge
+                 "split-merge-aao" split-merge-allatonce
+                 "split-merge-gray" split-merge-gray
+                 "split-non-am" split-non-ambiguity))
