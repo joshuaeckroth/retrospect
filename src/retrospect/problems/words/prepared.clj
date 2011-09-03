2dda0b42c4670dde41fcd56ba3a248b55963f73c
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-09-03 10:20:21 -0400

    Added first prepared action for words domain.

--- src/retrospect/problems/words/prepared.clj
+++ src/retrospect/problems/words/prepared.clj
@@ -1,3 +1,16 @@
-(ns retrospect.problems.words.prepared)
+(ns retrospect.problems.words.prepared
+  (:use [retrospect.problems.words.truedata :only [generate-truedata]])
+  (:use [retrospect.problems.words.sensors :only [generate-sensors]])
+  (:use [retrospect.random :only [set-seed]]))
 
-(def prepared-map {})
+(def random-1
+     (let [params {:Steps 500 :StepsBetween 20 :Threshold 25 :SensorNoise 20
+                   :BeliefNoise 0 :MaxModelGrams 3 :MetaStrategy :NoMetareasoning
+                   :Seed 10}]
+       (set-seed 10)
+       {:params params
+        :truedata (generate-truedata "data" params)
+        :sensors (generate-sensors params)}))
+
+(def prepared-map
+     (sorted-map "random-1" random-1))
