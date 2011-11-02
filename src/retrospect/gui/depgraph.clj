50f964c6c7796fcc3dfd5d643d1dca1b0b9edbf4
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-11-02 16:29:13 -0400

    Showing vertex labels in dependency graph.

--- src/retrospect/gui/depgraph.clj
+++ src/retrospect/gui/depgraph.clj
@@ -2,7 +2,11 @@
   (:import (edu.uci.ics.jung.graph DirectedSparseGraph))
   (:import (edu.uci.ics.jung.algorithms.layout DAGLayout))
   (:import (edu.uci.ics.jung.visualization GraphZoomScrollPane VisualizationViewer))
+  (:import (edu.uci.ics.jung.visualization.control DefaultModalGraphMouse))
+  (:import (edu.uci.ics.jung.visualization.control ModalGraphMouse$Mode))
+  (:import (edu.uci.ics.jung.visualization.decorators ToStringLabeller))
   (:import (java.awt.image BufferedImage))
+  (:import (java.awt Dimension))
   (:import (javax.swing JLabel ImageIcon JViewport))
   (:use [loom.graph :only [edges]])
   (:use [clj-swing.panel])
@@ -10,7 +14,9 @@
   (:use [retrospect.epistemicstates :only [draw-depgraph current-ep-state]]))
 
 (def layout (DAGLayout. (DirectedSparseGraph.)))
-(def view (VisualizationViewer. layout))
+(def view (doto (VisualizationViewer. layout)
+            (.setGraphMouse (doto (DefaultModalGraphMouse.)
+                              (.setMode ModalGraphMouse$Mode/TRANSFORMING)))))
 (def pane (GraphZoomScrollPane. view))
 
 (defn get-depgraph
@@ -23,10 +29,13 @@
 
 (defn depgraph-tab
   []
+  (.setVertexLabelTransformer (.getRenderContext view) (ToStringLabeller.))
   pane)
 
 (defn update-depgraph
   []
   (let [depgraph (:depgraph (current-ep-state (:ep-state-tree @or-state)))]
-    (.setGraphLayout view (DAGLayout. (get-depgraph depgraph)))
+    (.setGraphLayout view (doto (DAGLayout. (get-depgraph depgraph))
+                            (.setRepulsionRange 500)
+                            (.setSize (Dimension. 1000 1000))))
     (.stateChanged view (javax.swing.event.ChangeEvent. (Object.)))))
