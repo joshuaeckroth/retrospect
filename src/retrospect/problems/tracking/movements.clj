4a369a96f1b8075b82ebcd0ed4ba3b0221f778e9
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-11-02 22:11:29 -0400

    Added new movements.clj file.

---
+++ src/retrospect/problems/tracking/movements.clj
@@ -0,0 +1,103 @@
+(ns retrospect.problems.tracking.movements
+  (:use [retrospect.random]))
+
+;; Entity movements are stored in a map, whose keys are entity
+;; symbols. An entity symbol has metadata key :color. Also, the
+;; movements map has metadata keys :width, :height.
+;;
+;; For each entity, its movements are a vector of maps, where each
+;; map, except the first in the vector, has the keys :x, :y, :time,
+;; :ox, :oy, :ot; the first map has the keys :x, :y, :time.
+
+(defn new-movements
+  [width height]
+  (with-meta {} {:width width :height height}))
+
+(defn move-entity
+  [movements entity x y time]
+  (let [last-pos (last (get movements entity))]
+    (update-in movements [entity] conj
+               {:ox (:x last-pos) :oy (:y last-pos) :ot (:time last-pos) :x x :y y :time time})))
+
+(defn new-entity
+  [movements time]
+  (let [[x y] [(my-rand-int (:width (meta movements)))
+               (my-rand-int (:height (meta movements)))]
+        c (my-rand-nth [red blue green])
+        e (with-meta (symbol (str (count (keys movements)))) {:color c})]
+    (assoc movements e [{:x x :y y :time time}])))
+
+(defn calc-angle
+  [x y ox oy oox ooy]
+  (let [[dx1 dy1] [(- oox ox) (- ooy oy)]
+        [dx2 dy2] [(- x ox) (- y oy)]]
+    (if (or (and (= 0 dx1) (= 0 dy1))
+            (and (= 0 dx2) (= 0 dy2)))
+      ;; if no movement, just say it's 180-degrees
+      3.1415926
+      (Math/acos (/ (+ (* dx1 dx2) (* dy1 dy2))
+                    (* (Math/sqrt (+ (* dx1 dx1) (* dy1 dy1)))
+                       (Math/sqrt (+ (* dx2 dx2) (* dy2 dy2)))))))))
+
+(defn valid-angle?
+  [angle]
+  ;; angle is greater than 135-degrees
+  (<= (/ (* 135 3.1415926) 180.0) angle))
+
+(defn walk-rand
+  [[x y]]
+  (my-rand-nth [[x y] ;; don't move
+                [(dec x) y]
+                [(inc x) y]
+                [x (inc y)]
+                [x (dec y)]
+                [(dec x) (dec y)]
+                [(dec x) (inc y)]
+                [(inc x) (dec y)]
+                [(inc x) (inc y)]]))
+
+(defn walk
+  "Move an entity maxwalk steps in random directions, respecting angle constraints."
+  [movements entity time maxwalk]
+  (loop []
+    (let [last-last-pos (last (butlast (get movements entity)))
+          last-pos (last (get movements entity))
+          [x y] (loop [i (my-rand-int (inc maxwalk))
+                       loc [(:x last-pos) (:y last-pos)]]
+                  (if (= i 0) loc
+                      (recur (dec i) (walk-rand loc))))]
+      (if (and (< x (:width (meta movements))) (>= x 0)
+               (< y (:height (meta movements))) (>= y 0)
+               ;; don't check angle if the entity has not made two moves
+               (or (nil? last-last-pos)
+                   (valid-angle? (calc-angle x y (:x last-pos) (:y last-pos)
+                                             (:x last-last-pos) (:y last-last-pos)))))
+        (move-entity movements entity x y time)
+        (recur)))))
+
+(defn entity-movements
+  [movements entity mintime maxtime]
+  (filter #(and (>= (:time %) mintime) (<= (:time %) maxtime)) (get movements entity)))
+
+(defn set-movements
+  "Note: this function considers identical movements of distinct
+   entities to be equivalent; thus, as far as evaluation is concerned,
+   the tracker's accuracy scores are not penalized if it only believes
+   at least one of those movements."
+  [movements]
+  (set (apply concat (vals movements))))
+
+(defn entities-at
+  [movements x y time]
+  (filter (fn [e] (some (fn [mov] (and (= x (:x mov)) (= y (:y mov)) (= time (:time mov))))
+                        (get movements e)))
+          (keys movements)))
+
+(defn entities
+  [movements]
+  (keys movements))
+
+(defn dist
+  [x1 y1 x2 y2]
+  (double (math/sqrt (+ (* (- x1 x2) (- x1 x2))
+                        (* (- y1 y2) (- y1 y2))))))
