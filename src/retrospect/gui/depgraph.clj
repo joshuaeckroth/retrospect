0d94b3fa9b2315e0117bd1f429960f8f1a112a46
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-11-07 23:57:09 -0500

    Dependency graph is generated again, using SVG approach of hypothesis graph.

--- src/retrospect/gui/depgraph.clj
+++ src/retrospect/gui/depgraph.clj
@@ -1,9 +1,14 @@
 (ns retrospect.gui.depgraph
-  (:import (java.awt GridBagLayout Insets))
+  (:import (java.io StringReader))
+  (:import (java.awt GridBagLayout Insets Graphics Dimension Color))
   (:import (java.awt.image BufferedImage))
   (:import (javax.swing JLabel ImageIcon JViewport))
-  (:use [loom.graph :only [edges]])
+  (:import (org.apache.batik.dom.svg SAXSVGDocumentFactory))
+  (:import (org.apache.batik.swing JSVGCanvas))
+  (:import (org.apache.batik.util XMLResourceDescriptor))
+  (:use [loom.graph :only [edges nodes]])
   (:use [loom.io :only [dot-str]])
+  (:use [loom.attr :only [add-attr]])
   (:use [clojure.java.shell :only [sh]])
   (:use [clj-swing.core :only [add-action-listener]])
   (:use [clj-swing.panel])
@@ -11,31 +16,34 @@
   (:use [retrospect.state])
   (:use [retrospect.epistemicstates :only [current-ep-state]]))
 
-(defn get-depgraph
-  [depgraph]
-  (if (and depgraph (not-empty (edges depgraph)))
-    (let [dot (dot-str depgraph :graph {:dpi 60 :rankdir "LR"})
-          {png :out} (sh "dot" "-Tpng" :in dot :out-enc :bytes)]
-      (JLabel. (ImageIcon. png)))
-    (JLabel. (ImageIcon. (BufferedImage. 1 1 (. BufferedImage TYPE_4BYTE_ABGR))))))
-
-(def depgraph-scroll
-  (scroll-panel (get-depgraph nil)))
+(def canvas (JSVGCanvas.))
 
 (defn generate-depgraph
   []
   (let [depgraph (:depgraph (current-ep-state (:ep-state-tree @or-state)))]
-    (. depgraph-scroll setViewport
-       (doto (JViewport.) (.setView (get-depgraph depgraph))))))
+    (if (and depgraph (not-empty (edges depgraph)))
+      (let [dot (dot-str (reduce (fn [g n]
+                                   (-> g (add-attr n :label (:id n))
+                                       (add-attr n :id (:id n))))
+                                 depgraph (nodes depgraph))
+                         :graph {:dpi 60 :rankdir "LR"})
+            {svg :out} (sh "dot" "-Tsvg" :in dot)
+            sr (StringReader. svg)
+            parser (XMLResourceDescriptor/getXMLParserClassName)
+            doc (try (.createDocument (SAXSVGDocumentFactory. parser)
+                                      "file:///depgraph" sr)
+                     (catch Exception e (println e)))]
+        (.setDocumentState canvas JSVGCanvas/ALWAYS_DYNAMIC)
+        (.setDocument canvas doc)))))
 
 (defn depgraph-tab
   []
   (panel :layout (GridBagLayout.)
          :constrains (java.awt.GridBagConstraints.)
-         [:gridx 0 :gridy 0 :weightx 1.0 :weighty 1.0 :gridwidth 2
+         [:gridx 0 :gridy 0 :weightx 1.0 :weighty 1.0 :gridwidth 2 :fill :BOTH
           :insets (Insets. 5 5 5 5)
-          _ depgraph-scroll
-          :gridy 1 :gridwidth 1 :weightx 1.0 :weighty 0.0
+          _ (scroll-panel canvas)
+          :gridy 1 :gridwidth 1 :gridx 0 :weightx 1.0 :weighty 0.0
           _ (panel)
           :gridx 1 :weightx 0.0
           _ (doto (button "Generate")
