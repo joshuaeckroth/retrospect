7ef36f8f3be9db9164bb16aed4d77d5e70529870
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-11-28 09:03:22 -0500

    Reverting to clojure.test rather than lein.midje; also removed unused autodoc.

--- project.clj
+++ project.clj
@@ -9,10 +9,8 @@
                  [batik/batik-swing "1.6-1"]
                  [batik/batik-util "1.6-1"]
                  [batik/batik-dom "1.6-1"]]
-  :dev-dependencies [[midje "1.1"]
-                     [org.clojars.weavejester/autodoc "0.9.0"]
-                     [marginalia "0.7.0-SNAPSHOT"]
-                     [lein-marginalia "0.6.1"]]
+  :dev-dependencies [[marginalia "0.7.0-SNAPSHOT"]
+                     [lein-marginalia "0.7.0-SNAPSHOT"]]
   :main retrospect.core
   :keep-non-project-classes true
   :autodoc {:name "retrospect", :page-title "retrospect API" :trim-prefix "retrospect."
