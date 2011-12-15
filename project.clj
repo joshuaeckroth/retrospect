2301030b9073d0291ca32c98d86657d13d6b25c7
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-12-14 20:30:34 -0500

    Added radagast test coverage tool.

--- project.clj
+++ project.clj
@@ -10,10 +10,12 @@
                  [batik/batik-util "1.6-1"]
                  [batik/batik-dom "1.6-1"]]
   :dev-dependencies [[marginalia "0.7.0-SNAPSHOT"]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]]
+                     [lein-marginalia "0.7.0-SNAPSHOT"]
+                     [radagast "1.1.0"]]
   :main retrospect.core
   :keep-non-project-classes true
   :autodoc {:name "retrospect", :page-title "retrospect API" :trim-prefix "retrospect."
             :web-src-dir "https://github.com/joshuaeckroth/retrospect/blob/"}
   :java-source-path "src/jvm"
+  :radagast/ns-whitelist #"retrospect"
   :jvm-opts ["-Dawt.useSystemAAFontSettings=on" "-Xmx3000m" "-Xss4m"])
