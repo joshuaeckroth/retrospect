7bd243d2995b0570c44efcc4b1f82fa9b2fad8b7
Asher <balkin.4@osu.edu>
2012-02-08 16:10:54 -0500

    Removed dev libraries that are unnecessary and caused a problem in Windows.

--- project.clj
+++ project.clj
@@ -10,9 +10,7 @@
                  [batik/batik-gvt "1.6-1"]
                  [batik/batik-util "1.6-1"]
                  [batik/batik-dom "1.6-1"]]
-  :dev-dependencies [[marginalia "0.7.0-SNAPSHOT"]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]
-                     [radagast "1.1.0"]]
+
   :main retrospect.core
   :keep-non-project-classes true
   :java-source-path "src/jvm"
