57efffa35919c269544cae4b31762494a352e3e1
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-02-11 10:13:32 -0500

    Better bug reporting.

--- src/retrospect/bugreport.clj
+++ src/retrospect/bugreport.clj
@@ -1,6 +1,35 @@
 (ns retrospect.bugreport
-  (:import (org.bug4j.client Bug4jUncaughtExceptionHandler)))
+  (:import (java.awt GridBagLayout Insets))
+  (:import (java.io StringWriter PrintWriter))
+  (:import (org.bug4j.client Bug4jAgent))
+  (:use [clj-swing.panel])
+  (:use [clj-swing.button])
+  (:use [clj-swing.frame])
+  (:use [clj-swing.text-field]))
+
+(def exception (ref ""))
 
 (defn set-exception-handler
   []
-  (Bug4jUncaughtExceptionHandler/install))
+  (Thread/setDefaultUncaughtExceptionHandler
+   (proxy [Thread$UncaughtExceptionHandler] []
+     (uncaughtException [thread throwable]
+       (Bug4jAgent/report "Uncaught exception" throwable)
+       (dosync (alter exception (constantly
+                                 (let [s (StringWriter.)]
+                                   (.printStackTrace throwable (PrintWriter. s))
+                                   (str s)))))
+       (frame :title "Exception"
+              :layout (GridBagLayout.)
+              :constrains (java.awt.GridBagConstraints.)
+              :size [600 500]
+              :show true
+              :on-close :hide
+              [:gridx 0 :gridy 0 :weightx 1.0 :weighty 1.0
+               :fill :BOTH :insets (Insets. 5 5 5 5)
+               _ (scroll-panel (text-area :str-ref exception :editable false
+                                          :wrap false :rows 30))
+               :gridx 0 :gridy 1 :weighty 0.0
+               _ (button "Close app" :action ([_] (Bug4jAgent/shutdown)
+                                                (System/exit -1)))])
+       (Bug4jAgent/shutdown)))))
