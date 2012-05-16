4c662eb59990780b6cf99adf7e9129104e9f64cc
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-05-15 21:36:42 -0400

    Word hyp only explains merge hyps (not also split hyps).

--- src/retrospect/reason/abduction/problems/words/hypotheses.clj
+++ src/retrospect/reason/abduction/problems/words/hypotheses.clj
@@ -175,11 +175,15 @@
                        (new-hyp "Word" :word :word false conflicts
                                 (/ (double (get (:unigram-model kb) word))
                                    (double similar-sum))
-                                (filter #(or (= (dec (first pos-seq))
-                                                (:trans-pos %))
-                                             (= (last pos-seq)
-                                                (:trans-pos %)))
-                                        split-hyps)
+                                ;; a word explains the internal merge hyps
+                                ;; and the left/right split hyps (there may
+                                ;; be no left or right split hyp if this word
+                                ;; is at the start/end of the sentence)
+                                (filter #(and (< (:trans-pos %)
+                                                 (last pos-seq))
+                                              (>= (:trans-pos %)
+                                                  (first pos-seq)))
+                                        (sort-by :trans-pos merge-hyps))
                                 [] ;; no boosting
                                 word (format "Word: %s, pos-seq: %s\nsimilar: %s" word
                                              (str/join ", " (map str pos-seq))
