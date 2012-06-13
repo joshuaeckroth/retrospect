378f969d6b07ae5680833614e372717c04993082
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-06-13 11:11:58 -0400

    Added stopword filtering for classify domain. (Not sure if this is useful.)

--- src/retrospect/problems/classify/truedata.clj
+++ src/retrospect/problems/classify/truedata.clj
@@ -4,6 +4,19 @@
   (:use [retrospect.random])
   (:use [retrospect.state]))
 
+(def stopwords
+  #{"a" "able" "about" "across" "after" "all" "almost" "also" "am" "among"
+    "an" "and" "any" "are" "as" "at" "be" "because" "been" "but" "by" "can"
+    "cannot" "could" "dear" "did" "do" "does" "either" "else" "ever" "every"
+    "for" "from" "get" "got" "had" "has" "have" "he" "her" "hers" "him" "his"
+    "how" "however" "i" "if" "in" "into" "is" "it" "its" "just" "least" "let"
+    "like" "likely" "may" "me" "might" "most" "must" "my" "neither" "no" "nor"
+    "not" "of" "off" "often" "on" "only" "or" "other" "our" "own" "rather"
+    "said" "say" "says" "she" "should" "since" "so" "some" "than" "that" "the"
+    "their" "them" "then" "there" "these" "they" "this" "tis" "to" "too" "twas"
+    "us" "wants" "was" "we" "were" "what" "when" "where" "which" "while" "who"
+    "whom" "why" "will" "with" "would" "yet" "you" "your"})
+
 (defn generate-truedata
   []
   (let [doccats (reduce (fn [m line] (let [[docid _ & cats] (str/split line #"\s+")]
@@ -13,7 +26,8 @@
         docwords (reduce (fn [m docid]
                       (let [docdata (slurp (format "%s/classify/%s/%s.txt"
                                               @datadir (:Dataset params) docid))]
-                        (assoc m docid (set (str/split docdata #"\s+")))))
+                        (assoc m docid (set (filter #(not (stopwords %))
+                                               (str/split docdata #"\s+"))))))
                     {} (keys doccats))
         [training-docids testing-docids] (split-at (int (* (/ (:Knowledge params) 100.0)
                                                            (count docwords)))
