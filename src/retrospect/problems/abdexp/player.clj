12e197a1078f6fe0a58ce6e344f6dda6091c8821
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-06-27 20:19:02 -0400

    Support saving explanation graph in abdexp domain.

--- src/retrospect/problems/abdexp/player.clj
+++ src/retrospect/problems/abdexp/player.clj
@@ -1,18 +1,18 @@
 (ns retrospect.problems.abdexp.player
-  (:import (javax.swing JTabbedPane))
+  (:import (java.awt GridBagLayout Insets))
   (:use [loom.graph :only [digraph]])
+  (:use [clj-swing.core :only [add-action-listener]])
+  (:use [clj-swing.button])
   (:use [clj-swing.panel])
   (:use [retrospect.gui.graphs])
   (:use [retrospect.problems.abdexp.expgraph :only [format-dot-expgraph]])
   (:use [retrospect.epistemicstates :only [cur-ep]])
   (:use [retrospect.state]))
 
-(def canvases (ref {:initial nil
-                    :arbitrary nil
-                    :efli nil}))
+(def canvas (ref nil))
 
-(def current-hypgraph-dot (ref ""))
-(def current-hypgraph-svg (ref ""))
+(def current-expgraph-dot (ref ""))
+(def current-expgraph-svg (ref ""))
 
 (defn listener
   [node])
@@ -21,32 +21,28 @@
   []
   (panel))
 
-  (comment (when (< 0 @time-now)
-             (let [{:keys [arb efli]} (:workspace (cur-ep (:est @or-state)))]
-               (generate-graph (format-dot-expgraph (nth (:test @truedata) (dec @time-now)))
-                               (:initial @canvases) listener false)
-               (when arb
-                 (generate-graph (format-dot-expgraph arb)
-                                 (:arbitrary @canvases) listener false))
-               (when efli
-                 (generate-graph (format-dot-expgraph efli)
-                                 (:efli @canvases) listener false)))))
-
 (defn player-update-diagram
   []
   (if (< 0 @time-now)
     (generate-graph (format-dot-expgraph (get (:test @truedata) @time-now))
-                    (:initial @canvases) listener false
-                    current-hypgraph-dot current-hypgraph-svg)
-    (generate-graph (digraph) (:initial @canvases) listener false
-                    current-hypgraph-dot current-hypgraph-svg)))
+                    @canvas listener false
+                    current-expgraph-dot current-expgraph-svg)
+    (generate-graph (digraph) @canvas listener false
+                    current-expgraph-dot current-expgraph-svg)))
 
 (defn player-setup-diagram
   []
-  (dosync (alter canvases (constantly {:initial (create-canvas)
-                                       :arbitrary (create-canvas)
-                                       :efli (create-canvas)})))
-  (doto (JTabbedPane.)
-    (.addTab "Initial" (:initial @canvases))
-    (.addTab "Arbitrary" (:arbitrary @canvases))
-    (.addTab "EFLI" (:efli @canvases))))
+  (dosync (alter canvas (constantly (create-canvas))))
+  (panel :layout (GridBagLayout.)
+         :constrains (java.awt.GridBagConstraints.)
+         [:gridx 0 :gridy 0 :weightx 1.0 :weighty 1.0 :gridwidth 3 :fill :BOTH
+          :insets (Insets. 5 5 5 5)
+          _ @canvas
+          :gridy 1 :gridx 0 :weightx 1.0 :weighty 0.0 :gridwidth 1
+          _ (panel)
+          :gridx 1 :weightx 0.0
+          _ (doto (button "Save Dot")
+              (add-action-listener ([_] (save-dot @current-expgraph-dot))))
+          :gridx 2
+          _ (doto (button "Save SVG")
+              (add-action-listener ([_] (save-svg @current-expgraph-svg))))]))
