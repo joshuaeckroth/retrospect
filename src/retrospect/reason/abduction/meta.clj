14c645e2781c2a9894648db14ca1845cdbb029e1
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-08-28 13:56:01 -0400

    All metareasoning strategies fall back to ignoring noexp.

--- src/retrospect/reason/abduction/meta.clj
+++ src/retrospect/reason/abduction/meta.clj
@@ -37,6 +37,28 @@
   ;; we wanted to fix "no explainers"; so, did we?
   (empty? (get-no-explainers ws-new)))
 
+(defn ignore-hyp
+  [noexp workspace]
+  (add-kb workspace
+          [(new-hyp "Ignore" :kb :ignore 1.0 false
+                    (:conflicts?-fn noexp)
+                    []
+                    (format "Ignore %s" noexp)
+                    (format "Ignore %s" noexp)
+                    (:data noexp))]))
+
+(defn force-resolve-trigger
+  [truedata est time-prev time-now sensors]
+  (let [new-est (new-branch-ep est (cur-ep est))
+        new-ep (cur-ep new-est)
+        ws-old (:workspace new-ep)
+        noexp (map #(lookup-hyp ws-old %) (get-no-explainers ws-old))
+        ws-new (reduce (fn [ws h] (ignore-hyp h ws))
+                  (revert-workspace ws-old) noexp)
+        ws-expl (reason (when (:Oracle params) truedata) ws-new
+                        time-prev time-now sensors)]
+    (update-est new-est (assoc new-ep :workspace ws-expl))))
+
 (defn meta-apply-and-evaluate
   [truedata est new-est time-prev time-now sensors]
   (let [new-ep (cur-ep new-est)
@@ -51,30 +73,12 @@
       {:est new-expl-est
        :considered? true
        :accepted-branch? true}
-      {:est (goto-ep new-expl-est (:id (cur-ep est)))
+      ;; trigger for metareasoning not resolved; ignore the 'noexp' hyps
+      {:est (force-resolve-trigger
+             truedata (goto-ep new-expl-est (:id (cur-ep est)))
+             time-prev time-now sensors)
        :considered? true
        :accepted-branch? false})))
-
-(defn belief-revision
-  [noexp-hyp depth workspace]
-  ;; go back "before" this depth in order to repair
-  (revert-workspace workspace (dec depth)))
-
-(defn transitive-explanation
-  [noexp-hyp workspace]
-  ;; how to do this? this will explain unexplained-impasses, not
-  ;; noexp-impasses
-  workspace)
-
-(defn ignore-hyp
-  [noexp-hyp workspace]
-  (add-kb workspace
-          [(new-hyp "Ignore" :kb :ignore 1.0 false
-                    (:conflicts?-fn (:hyp noexp-hyp))
-                    []
-                    (format "Ignore %s" (:hyp noexp-hyp))
-                    (format "Ignore %s" (:hyp noexp-hyp))
-                    (:data (:hyp noexp-hyp)))]))
 
 (defn apply-resolutions
   [accepted est time-prev time-now sensors]
@@ -99,6 +103,17 @@
          :considered? true
          :accepted-branch? false}))))
 
+(defn belief-revision
+  [noexp-hyp depth workspace]
+  ;; go back "before" this depth in order to repair
+  (revert-workspace workspace (dec depth)))
+
+(defn transitive-explanation
+  [noexp-hyp workspace]
+  ;; how to do this? this will explain unexplained-impasses, not
+  ;; noexp-impasses
+  workspace)
+
 (defn make-noexp-hyps
   [noexp]
   (for [ne noexp]
@@ -115,23 +130,24 @@
   "Create explanations, and associated actions, for noexp."
   [ws-original noexp-hyps ws-depth time-now]
   ;; anomaly hyps
-  (concat
-   (mapcat (fn [ne] (for [i (range (dec ws-depth) 0 -1)]
-                     (new-hyp "Anomaly" :anomaly :anomaly
-                              (calc-doubt (revert-workspace ws-original i))
-                              false conflicts? [(:contents ne)]
-                              (format "%s is an anomaly from depth %d" ne i)
-                              (format "%s is an anomaly from depth %d" ne i)
-                              {:action (partial belief-revision ne i) :noexp-hyp ne
-                               :depth i})))
-           noexp-hyps)
-   (mapcat (fn [ne]
-             [(new-hyp "Noise" :noise :insertion-noise
-                       (+ 0.5 (* 0.5 (/ (double (:SensorInsertionNoise params)) 100.0)))
-                       false conflicts? [(:contents ne)]
-                       (format "%s is insertion noise" ne) (format "%s is insertion noise" ne)
-                       {:action (partial ignore-hyp ne) :noexp-hyp ne})])
-           (filter #(= :observation (:type (:hyp %))) noexp-hyps))))
+  (mapcat (fn [ne] (for [i (range (dec ws-depth) 0 -1)]
+                    (new-hyp "Anomaly" :anomaly :anomaly
+                             (calc-doubt (revert-workspace ws-original i))
+                             false conflicts? [(:contents ne)]
+                             (format "%s is an anomaly from depth %d" ne i)
+                             (format "%s is an anomaly from depth %d" ne i)
+                             {:action (partial belief-revision ne i) :noexp-hyp ne
+                              :depth i})))
+          noexp-hyps))
+
+(comment
+  (mapcat (fn [ne]
+            [(new-hyp "Noise" :noise :insertion-noise
+                      (+ 0.5 (* 0.5 (/ (double (:SensorInsertionNoise params)) 100.0)))
+                      false conflicts? [(:contents ne)]
+                      (format "%s is insertion noise" ne) (format "%s is insertion noise" ne)
+                      {:action (partial ignore-hyp (:hyp ne)) :noexp-hyp ne})])
+          (filter #(= :observation (:type (:hyp %))) noexp-hyps)))
 
 (comment
   (defn metareason
@@ -167,17 +183,21 @@
            attempted-depths #{}
            attempts 0]
       (cond (or (empty? hyps) (= attempts (:MetaBatchAttempts params)))
-            ;; apply noise hyps (give up)
-            (apply-resolutions (filter #(= :noise (:type %)) meta-hyps)
-                               est-meta time-prev time-now sensors)
-            ;; if we haven't attempted this depth yet
-            (not (attempted-depths (:depth (first hyps))))
+            ;; give up, force noexp hyps to be ignored
+            {:est (force-resolve-trigger
+                   truedata (goto-ep est-meta (:id (cur-ep est)))
+                   time-prev time-now sensors)
+             :considered? true
+             :accepted-branch? false}
+            ;; if we haven't attempted anything this deep
+            (every? #(> (:depth (first hyps)) %) attempted-depths)
             (let [result (apply-resolutions [(first hyps)]
                                             est-meta time-prev time-now sensors)]
               (if (:accepted-branch? result) result
                   (recur (:est result) (rest hyps)
                          (conj attempted-depths (:depth (first hyps))) (inc attempts))))
-            ;; else, already have attempted this depth; move on to next hyp
+            ;; else, already have attempted something this deep or
+            ;; more; move on to next hyp
             :else
             (recur est-meta (rest hyps) attempted-depths attempts)))))
 
