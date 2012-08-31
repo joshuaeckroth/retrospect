b40f184b73a5e40cbb1d90aa8c503bfd24eeaa3e
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-08-30 21:33:23 -0400

    Minor bugfix in explore mode.

--- src/retrospect/explore.clj
+++ src/retrospect/explore.clj
@@ -92,7 +92,7 @@
                          retrospect.state/params (assoc params :Seed seed)]
                  #_(dosync (alter log-box str (format "\nSeed: %d" seed)))
                  (let [truedata ((:generate-truedata-fn @problem))
-                       sensors ((:generate-sensors-fn @problem))
+                       sensors ((:generate-sensors-fn @problem) (:training truedata))
                        ors (init-ors sensors (:training truedata))
                        results (:results (cur-ep (:est (run-simulation truedata ors))))]
                    (dosync (alter retrospect.state/results conj (last results)))
