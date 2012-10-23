82085ff23b5a023f1d465724adc60d9ba8cd9a21
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-10-22 20:46:24 -0400

    Broke up a long function.

--- src/retrospect/problems/abdexp/truedata.clj
+++ src/retrospect/problems/abdexp/truedata.clj
@@ -21,7 +21,7 @@
 
 (defn new-observations
   []
-  (set (repeatedly (* (:Steps params) 10) #(format "O%d" (my-rand-int 10000)))))
+  (set (repeatedly (* (:Steps params) 2) #(format "O%d" (my-rand-int 10000)))))
 
 (defn new-explainers
   [vertices]
@@ -61,31 +61,81 @@
                      (get-in probs-parent-combs-map [pc v]))]
     (add-attr expgraph vertex :probs prob-table)))
 
+(defn random-score
+  [all-obs true-values f-obs v outgoing]
+  (cond (all-obs v) {"on" 1.0 "off" 0.0}
+        (true-values v)
+        (let [p (max 0.0 (min 1.0 (my-rand-gauss
+                                   (:TrueAprioriMean params)
+                                   (:TrueAprioriVariance params))))]
+          {"on" p "off" (- 1.0 p)})
+        ;; not true and explains
+        ;; a false observation;
+        ;; make it less likely
+        (not-empty (set/intersection outgoing (set f-obs)))
+        (let [p (max 0.0 (min 1.0 (my-rand-gauss
+                                   (/ (:FalseAprioriMean params) 2.0)
+                                   (:FalseAprioriVariance params))))]
+          {"on" p "off" (- 1.0 p)})
+        :else
+        (let [p (max 0.0 (min 1.0 (my-rand-gauss
+                                   (:FalseAprioriMean params)
+                                   (:FalseAprioriVariance params))))]
+          {"on" p "off" (- 1.0 p)})))
+
+(defn gen-explains-links
+  [attempts all-obs]
+  (loop [unexp all-obs
+         vs #{}
+         expl-links []]
+    (if (>= (count expl-links) (:NumExplainsLinks params))
+      (take (:NumExplainsLinks params) (my-shuffle expl-links))
+      ;; make some explainers for some of the unexplained
+      (let [to-be-explained (take (count unexp) (my-shuffle (sort unexp)))
+            reuse-n (if (< attempts 10) (:NumExplainers params) 0)
+            new-expl-links (mapcat (fn [v] (make-explainers vs v reuse-n))
+                                   to-be-explained)
+            newly-explained (set (map second new-expl-links))
+            newly-unexplained (set (map first new-expl-links))
+            new-unexp (set/union (set/difference unexp newly-explained) newly-unexplained)
+            new-vs (set/union vs newly-unexplained)]
+        (recur new-unexp new-vs (concat expl-links new-expl-links))))))
+
+(defn gen-observation-sets
+  [observations]
+  (loop [attempts 0]
+    (let [obs-choices (my-shuffle observations)
+          obs (repeatedly
+               (:UniqueTrueSets params)
+               ;; about 3 observations per step (have
+               ;; 10*steps to choose from, max)
+               #(take (* 3 (:Steps params)) obs-choices))]
+      ;; try 20 times to get :UniqueTrueSets number of different true sets
+      (if (and (< attempts 20) (not= (:UniqueTrueSets params)
+                                     (count (set obs))))
+        (recur (inc attempts)) obs))))
+
+(defn gen-conflicts-links
+  [true-values-maps all-obs eg vs]
+  (take (:NumConflictLinks params)
+        (my-shuffle
+         (sort #(let [c (compare (first %1) (first %2))]
+                  (if (= 0 c) (compare (second %1) (second %2)) c))
+               (set (filter (fn [[v1 v2]]
+                         (and (not= v1 v2)
+                              (or (not-any? #(% v1) true-values-maps)
+                                  (not-any? #(% v2) true-values-maps))
+                              (and (not (all-obs v1))
+                                   (not (all-obs v2)))
+                              (not (has-edge? eg v1 v2))
+                              (not (has-edge? eg v2 v1))))
+                       (combinations vs 2)))))))
+
 (defn random-expgraph
   []
   (loop [attempts 0]
     (let [all-obs (new-observations)
-          expl-links (loop [unexp all-obs
-                            vertices #{}
-                            expl-links []]
-                       (if (>= (count expl-links) (:NumExplainsLinks params))
-                         (take (:NumExplainsLinks params) (my-shuffle expl-links))
-                         ;; make some explainers for some of the unexplained
-                         (let [to-be-explained (take (count unexp)
-                                                     (my-shuffle (sort unexp)))
-                               new-expl-links (mapcat
-                                               (fn [v1] (make-explainers
-                                                        vertices v1
-                                                        (if (< attempts 10)
-                                                          (:NumExplainers params) 0)))
-                                               to-be-explained)
-                               newly-explained (set (map second new-expl-links))
-                               newly-unexplained (set (map first new-expl-links))
-                               new-unexp (set/union (set/difference unexp newly-explained)
-                                                newly-unexplained)
-                               new-vertices (set/union vertices newly-unexplained)]
-                           (recur new-unexp new-vertices
-                                  (concat expl-links new-expl-links)))))
+          expl-links (gen-explains-links attempts all-obs)
           eg (apply add-edges (digraph) expl-links)
           vs (sort (nodes eg))
           observations (sort ;; find the leaves (not just vertices
@@ -95,17 +145,7 @@
                                        (empty? (neighbors eg v))
                                        (not-empty (incoming eg v))))
                            vs))
-          observation-sets (loop [attempts 0]
-                             (let [obs-choices (my-shuffle observations)
-                                   obs (repeatedly
-                                        (:UniqueTrueSets params)
-                                        ;; about 3 observations per step (have
-                                        ;; 10*steps to choose from, max)
-                                        #(take (* 3 (:Steps params)) obs-choices))]
-                               ;; try 20 times to get :UniqueTrueSets number of different true sets
-                               (if (and (< attempts 20) (not= (:UniqueTrueSets params)
-                                                              (count (set obs))))
-                                 (recur (inc attempts)) obs)))
+          observation-sets (gen-observation-sets observations)
           ;; a true-values map for each observation-set; we're only putting "on" values in here
           true-values-maps (if (not (dag? eg)) []
                                (for [os observation-sets]
@@ -113,19 +153,7 @@
           ;; different false-obs for each eg-score/true-set
           false-obs (for [true-values true-values-maps]
                       (filter #(not (true-values %)) observations))
-          conflict-links (take (:NumConflictLinks params)
-                               (my-shuffle
-                                (sort #(let [c (compare (first %1) (first %2))]
-                                         (if (= 0 c) (compare (second %1) (second %2)) c))
-                                      (set (filter (fn [[v1 v2]]
-                                                (and (not= v1 v2)
-                                                     (or (not-any? #(% v1) true-values-maps)
-                                                         (not-any? #(% v2) true-values-maps))
-                                                     (and (not (all-obs v1))
-                                                          (not (all-obs v2)))
-                                                     (not (has-edge? eg v1 v2))
-                                                     (not (has-edge? eg v2 v1))))
-                                              (combinations vs 2))))))
+          conflict-links (gen-conflicts-links true-values-maps all-obs eg vs)
           eg-conflicts (reduce set-conflicts eg conflict-links)
           ;; a different eg-score for each true-set
           eg-scores (for [[true-values f-obs] (partition 2 (interleave true-values-maps false-obs))]
@@ -133,26 +161,8 @@
                            (-> eg
                               (add-attr v :id v)
                               (add-attr v :values ["on" "off"])
-                              (add-attr v :scores
-                                        (cond (all-obs v) {"on" 1.0 "off" 0.0}
-                                              (true-values v)
-                                              (let [p (max 0.0 (min 1.0 (my-rand-gauss
-                                                                         (:TrueAprioriMean params)
-                                                                         (:TrueAprioriVariance params))))]
-                                                {"on" p "off" (- 1.0 p)})
-                                              ;; not true and explains
-                                              ;; a false observation;
-                                              ;; make it less likely
-                                              (not-empty (set/intersection (neighbors eg v) (set f-obs)))
-                                              (let [p (max 0.0 (min 1.0 (my-rand-gauss
-                                                                         (/ (:FalseAprioriMean params) 2.0)
-                                                                         (:FalseAprioriVariance params))))]
-                                                {"on" p "off" (- 1.0 p)})
-                                              :else
-                                              (let [p (max 0.0 (min 1.0 (my-rand-gauss
-                                                                         (:FalseAprioriMean params)
-                                                                         (:FalseAprioriVariance params))))]
-                                                {"on" p "off" (- 1.0 p)})))))
+                              (add-attr v :scores (random-score all-obs true-values
+                                                                f-obs v (neighbors eg v)))))
                          eg-conflicts vs))
           eg-probs (map (fn [eg] (reduce add-prob-table eg vs)) eg-scores)
           bayesnets (map build-bayesnet eg-probs)]
