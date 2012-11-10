17e669ffc09f6dfc87aa58db8b8406a005a3a472
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-11-10 14:29:46 -0500

    Support updating apriori score when a hyp is added again.

--- src/retrospect/reason/abduction/workspace.clj
+++ src/retrospect/reason/abduction/workspace.clj
@@ -373,13 +373,15 @@
             (let [prior-hyp (lookup-hyp workspace prior-hyp-id)
                   prior-hyp-updated (assoc prior-hyp
                                       :needs-explainer? (:needs-explainer? hyp)
-                                      :explains (:explains hyp))
+                                      :explains (:explains hyp)
+                                      :apriori (:apriori hyp))
                   new-explains (set (map #(get (:hyp-contents workspace) %)
                                        (:explains prior-hyp-updated)))]
               (log hyp "is already in the workspace as" prior-hyp-updated
                    ", so updating what it explains:" new-explains)
               (let [ws (-> workspace
                           (forget-explainer prior-hyp-updated)
+                          (assoc-in [:hyp-ids prior-hyp-id] prior-hyp-updated)
                           (assoc-in [:explains prior-hyp-id] new-explains)
                           (assoc-explainer prior-hyp-updated)
                           (record-if-needs-explanation prior-hyp-updated))]
