7e3096a7cf17121626c51df2f3c8fca9c30b947e
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-04-12 10:51:39 -0400

    Put heap size in java opts again.

--- project.clj
+++ project.clj
@@ -25,5 +25,5 @@
   :main retrospect.core
   :keep-non-project-classes true
   :java-source-paths ["src/jvm"]
-  :jvm-opts ["-Dawt.useSystemAAFontSettings=on" "-Djava.library.path=netica"])
+  :jvm-opts ["-Xmx4000m" "-Dawt.useSystemAAFontSettings=on" "-Djava.library.path=netica"])
 
