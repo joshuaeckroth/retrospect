78aba58a67d8fdd9f621f717bad4cd1a6e63a2e8
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-04-11 20:39:40 -0400

    Workspace tests regarding composite hyps.

--- test/retrospect/reason/abduction/workspace_test.clj
+++ test/retrospect/reason/abduction/workspace_test.clj
@@ -234,3 +234,64 @@
       (is (= [6 7] (get-no-explainers ws-expl)))
       (is (= 0.5 (get-noexp-pct ws-expl)))
       (is (= #{3 4 5} (find-unaccepted ws-expl))))))
+
+(deftest test-composite
+  (dosync (alter reasoner (constantly reason-abduction))
+          (alter problem (constantly abdexp-problem)))
+  (binding [last-id 0
+            params (assoc (get-default-params) :simulation 0)]
+    (let [conflicts?-fn (fn [hyp1 hyp2] (and (not= hyp1 hyp2)
+                                            (= (:type hyp1) (:type hyp2))))
+          ;; id 1
+          e1 (new-hyp "Test" :explained :mysubtype 0.5 true
+                      (constantly false) [] "" "" {:a 1})
+          ;; id 2
+          e2 (new-hyp "Test" :explained :mysubtype 0.5 true
+                      (constantly false) [] "" "" {:a 2})
+          ;; id 3
+          h1 (new-hyp "Test" :type1 :mysubtype 0.25 false
+                      conflicts?-fn [(:contents e1)]
+                      "short-descr" "desc" {:x 1})
+          ;; id 4
+          h2 (new-hyp "Test" :type2 :mysubtype 0.50 false
+                      conflicts?-fn [(:contents e2)]
+                      "short-descr" "desc" {:x 2})
+          ;; id 5
+          h3 (new-hyp "Test" :type2 :mysubtype 0.60 false
+                      conflicts?-fn [(:contents e2)]
+                      "short-descr" "desc" {:x 3})
+          ;; id 6 (a composite of h1 and h2)
+          hc1 (new-composite "TestComp" :type-comp1 :mysubtype 0.3
+                             [(:contents e1) (:contents e2)]
+                             "" "" {:c 1} [h1 h2])
+          ws (-> (init-workspace)
+                (add-observation e1 1)
+                (add-observation e2 1)
+                (add h3 1)
+                (add hc1 1))
+          ws-expl (explain ws 1 1)
+          ws-rej (-> ws (add h2 1) (reject-many [h2] :conflict 1))
+          ws-rej-cleanup (clean-up-workspace ws-rej 1)
+          ws-rej-expl (explain ws-rej 1 1)]
+      (is (:composite? hc1))
+      (is (= [h1 h2] (:hyps hc1)))
+      (is (= [e2] (explains ws h3)))
+      (is (= [e1 e2] (explains ws hc1)))
+      (is (= [hc1] (explainers ws e1)))
+      (is (= [h3 hc1] (explainers ws e2)))
+      ;; after explaining
+      (is (accepted? ws-expl e1))
+      (is (accepted? ws-expl e2))
+      (is (accepted? ws-expl hc1))
+      (is (accepted? ws-expl h1))
+      (is (accepted? ws-expl h2))
+      (is (rejected? ws-expl h3))
+      ;; rather than explaining, a preemptive rejection of a hyp in the composite
+      (is (rejected? ws-rej h2))
+      (is (not (rejected? ws-rej hc1)))
+      (is (rejected? ws-rej-cleanup h2))
+      (is (rejected? ws-rej-cleanup hc1))
+      (is (= :conflict (rejection-reason ws-rej-cleanup hc1)))
+      (is (rejected? ws-rej-expl h2))
+      (is (rejected? ws-rej-expl hc1))
+      (is (accepted? ws-rej-expl h3)))))
